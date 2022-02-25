426ba3d179d7a83f5ee040e525b975e295fdc887
fred <domaine75@gmail.com>
2022-02-25 23:15:42 +0100

    Add dotnet build method

--- Deploy/Program.cs
+++ Deploy/Program.cs
@@ -13,14 +13,21 @@ namespace Deploy
       Display("Deployment of application with GIT");
       Display("cloning a repo");
       string gitRepo = "https://github.com/fredatgithub/Matrix";
-      string gitRepoDirectory = @"c:\temp";
-      if (GitClone(gitRepoDirectory, gitRepo))
+      string gitRepoDirectoryInitial = @"c:\temp";
+      string gitRepoDirectoryFinal = @"c:\temp\matrix";
+      if (GitClone(gitRepoDirectoryInitial, gitRepo))
       {
-        Display($"The git repo: {gitRepo} has been cloned in the directory: {gitRepoDirectory}");
+        Display($"The git repo: {gitRepo} has been cloned in the directory: {gitRepoDirectoryInitial}");
       }
       else
       {
-        Display($"The git repo: {gitRepo} has not been cloned in the directory: {gitRepoDirectory}");
+        Display($"The git repo: {gitRepo} has not been cloned in the directory: {gitRepoDirectoryInitial}");
+        return;
+      }
+
+      if (DotNetBuild(gitRepoDirectoryFinal))
+      {
+        Display($"The local repo {gitRepoDirectoryFinal} has been built");
       }
 
       Display("Press any key to exit:");
@@ -51,7 +58,6 @@ namespace Deploy
           powershell.AddScript(@"git add *");
           powershell.AddScript($@"git commit -m '{message}'");
           powershell.AddScript(@"git push");
-
           Collection<PSObject> results = powershell.Invoke();
           result = true;
         }
@@ -84,10 +90,43 @@ namespace Deploy
         using (PowerShell powershell = PowerShell.Create())
         {
           powershell.AddScript($"cd {directoryPath}");
-
           powershell.AddScript($@"git clone {gitPath}");
           Collection<PSObject> results = powershell.Invoke();
           result=true;
+        }
+      }
+      catch (Exception)
+      {
+        result = false;
+      }
+
+      return result;
+    }
+
+    private static bool DotNetBuild(string directoryPath)
+    {
+      bool result = true;
+      try
+      {
+        if (!Directory.Exists(directoryPath))
+        {
+          Directory.CreateDirectory(directoryPath);
+        }
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+
+      try
+      {
+        using (PowerShell powershell = PowerShell.Create())
+        {
+          powershell.AddScript($"cd {directoryPath}");
+          powershell.AddScript("dotnet restore");
+          powershell.AddScript("dotnet build");
+          Collection<PSObject> results = powershell.Invoke();
+          result = true;
         }
       }
       catch (Exception)
